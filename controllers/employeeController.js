2be4f2a37e25b1eb2cfa18714c4b3be4e99297fa
Roflianochka <maximbelaev831@gmail.com>
2023-12-07 20:48:09 +0300

    feat: add route files corkas and contraoller files corkas

---
+++ controllers/employeeController.js
@@ -0,0 +1,12 @@
+class EmployeeController{
+    async create(req,res){
+
+    }
+    async getAll(req,res){
+        
+    }
+    async getOne(req,res){
+        
+    }
+}
+module.exports = new EmployeeController()
